a80acc2f2d3cae483a91062b560cf8cae834e86c
Hampeeeeeee <hampus_svensson99@hotmail.com>
2025-09-01 13:15:52 +0200

    React Introduktion

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Button from "./components/Button"
+
+function App() {
+
+  return (
+    <>
+    <h1>Min första react app</h1>
+    <Button />
+    </>
+  )
+}
+
+export default App;
---
+++ vite-project/src/components/Button.tsx
@@ -0,0 +1,10 @@
+const Button = () => {
+    return (
+        <>
+        <button>Button 1</button>
+        <button>Button 2</button>
+        </>
+    )
+    }
+
+export default Button;
